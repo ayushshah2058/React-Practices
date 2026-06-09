085b30875e18461f3ebc64e91ab31e3e5a71d9de
Ayush shah <120931510+ayushshah2058@users.noreply.github.com>
2026-06-09 12:53:20 +0000

    new app component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,32 +4,19 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+function Title() {
+  return <h1>I'm the title</h1>;
+}
 function App() {
 
   return (
-    export const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <ul>
-        <li className="nav-item">
-          <a href='#'>Dashboard</a>
-        </li>
-        <li className="nav-item">
-          <a href="#">Widgets</a>
-        </li>
-        <li className="nav-item">
-          <button aria-expanded="false">Apps</button>
-          <ul className="sub-menu" aria-label="Apps">
-            <li><a href="#">Calendar</a></li>
-            <li><a href="#">Chat</a></li>
-            <li><a href="#">Email</a></li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <Title/>
+      <button>I;m Button</button>
+      <p>I;m P</p>
+    </div>
   )
-}
-  )
+
 }
 
 export default App
