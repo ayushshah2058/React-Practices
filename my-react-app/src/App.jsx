5e06e45c3f3fa173b3e713bd4ebb12061403d316
Ayush shah <120931510+ayushshah2058@users.noreply.github.com>
2026-06-09 12:36:14 +0000

    First react commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="nav-item">
+          <a href='#'>Dashboard</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Widgets</a>
+        </li>
+        <li className="nav-item">
+          <button aria-expanded="false">Apps</button>
+          <ul className="sub-menu" aria-label="Apps">
+            <li><a href="#">Calendar</a></li>
+            <li><a href="#">Chat</a></li>
+            <li><a href="#">Email</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+  )
+}
+
+export default App
